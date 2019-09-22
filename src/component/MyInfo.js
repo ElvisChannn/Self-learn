c6288361ca11802e5677c4ca560d24e059e52957
elvischan <elvis.chan@mediatropy.com>
2019-09-22 10:08:10 +0800

    added js file

---
+++ src/component/MyInfo.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+function MyInfo(){
+    return(
+        <div>
+            <h1>Elvis Chan</h1>
+            <p>I am learning reactjs</p>
+            <ul>
+                <li>HKCC</li>
+                <li>UST</li>
+                <li>SKHLMCSS</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MyInfo
